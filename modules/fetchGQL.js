5573536ef7eaf2fcbd34d6a9258e6a6064ce8ad1
fcortesbio <fcortesbio@gmail.com>
2024-12-16 23:04:45 -0500

    updated fetchGQL

--- modules/fetchGQL.js
+++ modules/fetchGQL.js
@@ -6,7 +6,7 @@ async function fetchGraphQL(query, variables) {
       
       headers: 
       {
-        "Content-Type": "application/json";
+        "Content-Type": "application/json"
       },
       
       body: JSON.stringify({
@@ -23,15 +23,15 @@ export async function getData(startId, endId, batchSize = 20) {
   const pokemonData = [];
   endId = endId || startId;
 
-  // Log the fetch operation
-  endId === startId?
-  console.log(`Fetching data for Pokemon ID=${endId}`):
-  console.log(`Fetching data for Pokémon from ID=${startId} to ID=${endId}`);
+  console.log(
+    endId === startId
+      ? `Fetching data for Pokemon ID=${endId}`
+      : `Fetching data for Pokémon from ID=${startId} to ID=${endId}`
+  );
 
-  // Construct the GraphQL query
   const query = `
     query pokemon_details($startId: Int!, $endId: Int!) {
-      pokemon_v2_pokemon(where: {id: {_between: [$startId, $endId]}}) {
+      pokemon_v2_pokemon(where: { id: { _gte: $startId, _lte: $endId } }) {
         id
         name
         types: pokemon_v2_pokemontypes {
@@ -44,22 +44,22 @@ export async function getData(startId, endId, batchSize = 20) {
   `;
 
   try {
-    // Fetch the data using the GraphQL query
     const response = await fetchGraphQL(query, { startId, endId });
     console.log("GraphQL Response:", response);
 
-  const { data } = response;
+    const { data } = response;
 
-    const { data } = await fetchGraphQL(query, { startId, endId });
-
-    // Extract the relevant data
-    for (const pokemon of data.pokemon_v2_pokemon) {
-      const filteredData = {
-        id: pokemon.id,
-        name: pokemon.name,
-        types: pokemon.types.map((typeData) => typeData.type.name),
-      };
-      pokemonData.push(filteredData);
+    if (data && data.pokemon_v2_pokemon) {
+      for (const pokemon of data.pokemon_v2_pokemon) {
+        const filteredData = {
+          id: pokemon.id,
+          name: pokemon.name,
+          types: pokemon.types.map((typeData) => typeData.type.name),
+        };
+        pokemonData.push(filteredData);
+      }
+    } else {
+      console.error("No data returned for the specified query.");
     }
   } catch (error) {
     console.error("Error fetching data:", error);
